cb08fcec4f2f21a48ee05b4faa5ccb076654149e
yuliu <liuyujie1994@126.com>
2017-11-01 09:31:42 +0800

    风味食堂页面后台增加时间档处理/添加关于页面后台

---
+++ src/digitalCanteenSSM/controller/AboutController.java
@@ -0,0 +1,18 @@
+package digitalCanteenSSM.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AboutController {
+	@RequestMapping("/aboutPage")
+	public ModelAndView aboutPage() throws Exception{
+		ModelAndView modelAndView = new ModelAndView();
+		
+		modelAndView.setViewName("/WEB-INF/jsp/m_aboutPage.jsp");
+		
+		return modelAndView;
+	}
+	
+}
